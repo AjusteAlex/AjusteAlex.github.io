05ebac4186c4140c39011a87a2eee186c544eb53
AjusteAlex <alexandre.ajuste@gmail.com>
2020-11-27 13:28:55 +0100

    scroll page ok

--- js/main-menu.js
+++ js/main-menu.js
@@ -1,5 +1,5 @@
 
-
+// ajout background menu lors du scroll
 document.querySelector('.closebtn').addEventListener('click', function(){
   document.querySelector(".main-menu").style.right = "-100%";
 });
@@ -8,17 +8,14 @@ document.querySelector('.openbtn').addEventListener('click', function(){
 });
 
 
-
-// https://bootsnipp.com/snippets/kl8Q3
-// http://jsfiddle.net/4xSe4/5/
-
-$(window).scroll(function() {    
-  var scroll = $(window).scrollTop();
-  if (scroll != 0) {
-      $(".main-menu").addClass("scrolling").css('transition', '0.5s');
-  } else {
-      $(".main-menu").removeClass("scrolling");
-  } 
+//scroll page
+$(document).ready(function() {
+  $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+    var page = $(this).attr('href'); // Page cible
+    var speed = 750; // Durée de l'animation (en ms)
+    $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+    return false;
+  });
 });
 
 
