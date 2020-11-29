de36da687c5c0b7eec51e2dd36e3735e5ea26531
AjusteAlex <alexandre.ajuste@gmail.com>
2020-11-29 14:23:21 +0100

    remove transition menu mob->dekt+add required form

--- php/contact.php
+++ php/contact.php
@@ -1,14 +1,8 @@
 
 <?php
-  if(isset($_POST['submit'])) {
-    try {
-        $bdd_connect = new PDO('mysql:host=localhost;dbname=portfolio', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-    } catch (Exception $error) {
-        die('ERROR: ' . $error->getMessage());
-    }
+ include 'connexion.php';
 
-
-    if ($_POST["name"] != "" && $_POST["email"] != "" && $_POST["message"] != "") {
+    if ($_POST["name"] != "" && $_POST["email"] != "" && $_POST["message"] != "" && isset($_POST['rgpd'])) {
 
         //Tout est ok, enregistrement en base de donnees est autorisée//
         $sql="INSERT INTO `formulaire_contact` (`id_message`, `name`, `email`, `message`) VALUES (NULL, '" . $_POST["name"] . "', '" . $_POST["email"] . "', '" . $_POST["message"] . "' )";
@@ -19,7 +13,8 @@
         header("Location: http://localhost/portfolio/#contact");
 
     } else {
-        header("Location: http://localhost/portfolio/error404.html ");
+        header("Location: http://localhost/portfolio/error404.html");
+
     }
-}
+
 ?>
