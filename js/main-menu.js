65f4926188a8f937476a2cb4e027536b2ac2c051
AjusteAlex <alexandre.ajuste@gmail.com>
2020-12-02 15:23:45 +0100

    add cv && delete hont h3.js

--- js/main-menu.js
+++ js/main-menu.js
@@ -1,16 +1,28 @@
 
 // menu mobile
-document.querySelector('.closebtn').addEventListener('click', function(){
-  document.querySelector(".main-menu").style.right = "-100%";
+$(document).ready(function () {
+  menu = document.querySelector(".main-menu"); // querySelector permet de selectionner la class voulu 
+  openbtn = document.querySelector('.openbtn'); 
+  closebtn = document.querySelector('.closebtn');
+
+  openbtn.addEventListener('click', function(){ // addEventListener permet d'appeler l'événement (click) spécifié est d'envoyé vers la cible.
+    $(".openbtn").css('display', 'none');
+    menu.style.right = "0";
+  });
+  
+  closebtn.addEventListener('click', function(){
+    $(".openbtn").css('display', 'block');
+    menu.style.right = "-100%";
+  });
 });
-document.querySelector('.openbtn').addEventListener('click', function(){
-  document.querySelector(".main-menu").style.right = "0";
-});
+
+
+
 
 
 // ajout background menu lors du scroll
-$(window).scroll(function() {    
-  var scroll = $(window).scrollTop();
+$(window).scroll(function() {   //L’évènement scroll est émis lorsque l’on fait défiler le document.  
+  scroll = $(window).scrollTop(); // scrolltop, nombre de pixel par rapport au haut de la page
   if (scroll != 0) {
       $(".main-menu").addClass("scrolling").css('transition', '0.5s');
   } else {
@@ -22,9 +34,9 @@ $(window).scroll(function() {
 //scroll page
 $(document).ready(function() {
   $('.js-scrollTo').on('click', function() { // Au clic sur un élément
-    var page = $(this).attr('href'); // Page cible
-    var speed = 750; // Durée de l'animation (en ms)
-    $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+    page = $(this).attr('href'); // Page cible
+    speed = 750; // Durée de l'animation (en ms)
+    $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); 
     return false;
   });
 });
