47c27d2d6247246cb726083b214ebf15c56bc8d3
AjusteAlex <alexandre.ajuste@gmail.com>
2020-12-07 15:59:56 +0100

    rendu projet

--- php/contact.php
+++ php/contact.php
@@ -6,6 +6,7 @@
 
         //Tout est ok, enregistrement en base de donnees est autorisée//
         $sql="INSERT INTO `formulaire_contact` (`id_message`, `name`, `email`, `message`) VALUES (NULL, '" . $_POST["name"] . "', '" . $_POST["email"] . "', '" . $_POST["message"] . "' )";
+
         $req = $bdd_connect->prepare($sql) or die(print_r($bdd_connect->errorInfo()));
         $req->execute();
         $req->closeCursor();
